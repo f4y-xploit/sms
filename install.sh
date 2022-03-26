73ec8db24fb81ddef71532b79cfc8cd5021d46fe
F4y Xploit <100696540+f4y-xploit@users.noreply.github.com>
2022-03-26 08:50:29 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+clear
+
+# AUTHOR : F4Y XPLOIT
+# TEAM   : BITUNG BLACKHAT
+# MESSAGE: LU BABI
+sleep 1
+
+echo '
+-> Welcome to installation package <-
+'
+sleep 1
+echo
+echo '
+[-] Wait a 3 secoond [-]
+'
+sleep 3
+echo
+echo '[!]  started '
+sleep 1
+
+pkg install python
+pkg install python2
+pkg install ruby
+pkg install pip
+pip2 install mechanize
+gem install lolcat
+pkg install figlet
+
+echo
+echo
+echo
+sleep 1
+echo ' [!] FINISHED -> BY F4Y XPLOIT
+sleep 1'
+echo ' [!] Follow IG -> f4y_xploit'
+figlet " DONE "|lolcat
+sleep 1
+echo' :: Thanks for F4Y XPLOIT'
